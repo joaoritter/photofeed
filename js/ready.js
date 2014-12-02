792b7946c60ca6dd389b6c08dbcafc0594edf269
joaoritter <ritterj@cs.unc.edu>
2014-12-02 07:06:41 -0500

    with form

--- js/ready.js
+++ js/ready.js
@@ -23,12 +23,17 @@ var photo_insertion_duration_mobile = 300; //the time it takes for each photo to
 
 $(document).ready(function() {
     createTroubleshootMessage();
+    handleForm();
     addName();
     createPhotoGallery();
 });
 
+var timeout;
 var createTroubleshootMessage = function() {
-    setTimeout(function(){$("#troubleshoot_message").css('visibility', 'visible')}, 6000);
+    clearTimeout(timeout);
+    var message = $("#troubleshoot_message");
+    message.css('visibility', 'hidden');
+    timeout = setTimeout(function(){ message.css('visibility', 'visible')}, 6000);
 }
 
 var addName = function() {
@@ -37,12 +42,108 @@ var addName = function() {
     container.append(artist.name);
 }
 
-var createPhotoGallery = function(callback) {
+var handleForm = function() {
+    var flickr_id_input = $("input[name='flickr_id']"); 
+    var name_input = $("input[name='name']");
+
+    flickr_id_input.val(function() { return artist.flickr_id });
+    name_input.val(function() { return artist.name });
+    flickr_id_input.focus();
+
+    var circle = $("#circle"); 
+    var form = $("#form");
+    var banner = $(".banner");
+    var gallery = $(".gallery");
+    circle.mouseenter(function() {
+        form.fadeIn();   
+    });
+
+    banner.on("mouseenter", function() {
+        form.fadeOut("2000"); 
+    });
+    gallery.on("mouseenter", function() {
+        form.fadeOut("2000"); 
+    });
+    
+    var down = false;
+    $("#form input").keydown(function (e) {
+        if (down) { return;}
+        down = true;
+        var key = e.which;
+        if(key == 13){
+            validateForm();
+        }
+    });
+
+    $("#form input").keyup(function (e) {
+        down = false; 
+    });
+}
+
+var validateForm = function() {
+    var flickr_id = $("input[name='flickr_id']").val() 
+    var name = $("input[name='name']").val() 
+    console.log(flickr_id, name);
+    if (!flickr_id.length || !name) {
+        return; 
+    }
+    var new_user = {
+        "name": name,
+        "flickr_id": flickr_id
+    }
+    artist = new_user;
+    updatePhotoGallery();
+}
+
+var updatePhotoGallery = function() {
+    getPhotos(function(photos) {
+        var photo_gallery_selector = '#photo_container';
+        var gallery = $(photo_gallery_selector);
+        var imgs_html = photos.join("");
+        refreshTitle();
+        gallery.remove();
+        createTroubleshootMessage();
+        addLoading(function() {
+            $('.main_column').append('<div id="photo_container" class="center-block"></div>');
+            gallery = $(photo_gallery_selector);
+            gallery.append(imgs_html);
+            gallery.stalactite({
+                duration: photo_insertion_duration,
+                easing: 'swing', 
+                cssPrep: true,
+                loader: '<img />',
+                fluid: false   
+            });
+            gallery.fadeIn();
+        });
+    });
+}
+
+var addLoading = function(callback) {
+    $('.loading').fadeIn("slow", function() { callback()}); 
+}
+
+var createPhotoGallery = function() {
+    getPhotos(function(photos) {
+        var photo_gallery_selector = '#photo_container';
+        var gallery = $(photo_gallery_selector);
+        var imgs_html = photos.join("");
+        gallery.append(imgs_html);
+        display(gallery);
+    });
+}
+
+var getPhotos = function(callback) {
     var key = '4fdc983965119bdf7a0832b9e93ec19f';
     var user_id = artist.flickr_id;
     var extras = 'url_c';
     var url = 'https://api.flickr.com/services/rest/?method=flickr.people.getPublicPhotos&api_key=' + key + '&user_id=' + user_id + '&extras=' + extras + '&per_page=100&page=1&format=json&nojsoncallback=1'
-    $.getJSON(url, function(data) {
+    console.log(url);
+    $.getJSON(url, function(data) { 
+        if (data.code == 1) {  
+            handleInvalid(); 
+            return;
+        }
         var photos = []; 
         var photo_objs = data.photos.photo
         $.each(photo_objs, function(key, photo_obj) {
@@ -50,11 +151,25 @@ var createPhotoGallery = function(callback) {
             photos.push(photo);
         });
         if (should_photos_shuffle) { shuffle(photos); }
-        var imgs_html = photos.join("");
-        var photo_gallery_selector = '#photo_container';
-        var gallery = $(photo_gallery_selector);
-        gallery.append(imgs_html);
-        display(gallery);
+        callback(photos) 
+    })
+}
+
+var handleInvalid = function() { 
+    $('#error').fadeIn(50);
+    $("#form input").keydown(function (e) {
+        $('#error').fadeOut(50); 
+    });
+}
+
+var refreshTitle = function() {
+    var title_selector = '#artists_name';
+    var title = $(title_selector); 
+    title.fadeOut('100', function() {
+        title.text(function() {
+            return artist.name; 
+        });
+        title.fadeIn('100');
     });
 }
 
@@ -70,7 +185,6 @@ var generateSize = function() {
     var window_width = $(window).width();
     //make quasi-random size
     var random = Math.random()
-
     if (window_width > 1200) {
         if (random < .3) { size = '' } //30% 500px
         else if (random < .6) { size = '_z' } //30% 640px
